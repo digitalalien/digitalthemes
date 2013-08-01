a15b72a56ac86255ddc416ec90ebf4af506978dc
Andrew Mallonee <abmallonee@yahoo.com>
2013-08-01 18:23:30 -0400

    init

---
+++ wp-content/themes/K.I.S.S.it/header.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * The Header for our theme.
+ *
+ * Displays all of the <head> section and everything up till <div id="main">
+ *
+ * @package K.I.S.S.it
+ */
+?><!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+<meta charset="<?php bloginfo( 'charset' ); ?>">
+<meta name="viewport" content="width=device-width">
+<title><?php wp_title( '|', true, 'right' ); ?></title>
+<link rel="profile" href="http://gmpg.org/xfn/11">
+<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+
+<?php wp_head(); ?>
+</head>
+
+<body <?php body_class(); ?>>
+<div id="page" class="hfeed site">
+	<header id="masthead" class="site-header" role="banner">
+		<hgroup></hgroup>
+		<nav role="navigation" class="site-navigation main-navigation"></nav>
