53f8a2d0741e8b7fd9a328e37689accba7ccf8bd
Andrew Mallonee <abmallonee@yahoo.com>
2013-08-03 10:41:34 -0400

    before go back to original

--- wp-content/themes/K.I.S.S.it/header.php
+++ wp-content/themes/K.I.S.S.it/header.php
@@ -5,23 +5,62 @@
  * Displays all of the <head> section and everything up till <div id="main">
  *
  * @package K.I.S.S.it
+ * @since K.I.S.S.it 1.0
  */
 ?><!DOCTYPE html>
-<html <?php language_attributes(); ?>>
+<!--[if IE 8]>
+<html id="ie8" <?php language_attributes(); ?>>
+<![endif]-->
+<!--[if !(IE 8)]><!-->
+<html <?php language_attributes();?>>
+<!--<![endif]-->
 <head>
 <meta charset="<?php bloginfo( 'charset' ); ?>">
 <meta name="viewport" content="width=device-width">
-<title><?php wp_title( '|', true, 'right' ); ?></title>
-<link rel="profile" href="http://gmpg.org/xfn/11">
-<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
-
+<title>
+    <?php
+        /**
+         * Print the title tag based on what is being viewed
+         */
+         global $page, $paged;
+         wp_title(' | ', true, 'right');
+         
+        //add the blog name.
+        bloginfo('name');
+        
+        //add the blog description for the home/front page.
+        $site_description = get_bloginfo('description', 'display');
+        if($site_description && (is_home() || is_front_page())){
+            echo ' | ' . sprintf(__('Page %s', 'k_i_s_s_it'), max($paged, $page));
+        }
+    ?>
+</title>
+<link rel="profile" href="http://gmpg.org/xfn/11"/>
+<link rel="pingback" href="<?php bloginfo('pingback_url');?>"/>
+<!--[if v IE 9]>
+<script src="<?php echo get_template_directory_uri();?>/js/html5.js" type="text/javascript"></script>
+<![endif]-->
 <?php wp_head(); ?>
 </head>
 
 <body <?php body_class(); ?>>
 <div id="page" class="hfeed site">
 	<header id="masthead" class="site-header" role="banner">
-		<hgroup></hgroup>
-		<nav role="navigation" class="site-navigation main-navigation"></nav>
+		<hgroup>
+            <h1 class="site-title">
+                <a href="<?php echo home_url('/');?>" title="<?php echo esc_attr(get_bloginfo('name', 'display'));?>" rel="home"><?php bloginfo('name');?></a>
+            </h1>
+            <h2 class="site-description"><?php bloginfo('description');?></h2>
+        </hgroup>
+        
+		<nav role="navigation" class="site-navigation main-navigation">
+            <h1 class="assistive-text">
+                <?php _e('Menu', 'k_i_s_s_it');?>
+            </h1>
+            <div class="assistive-text skip-link">
+                <a href="#content" title="<?php esc_attr_e('Skip to content', '_s');?>"><?php _e('Skip to content', 'k_i_s_s_it');?></a>
+            </div>
+            <?php wp_nav_menu(array('theme_location' => 'primary'));?>
+        </nav><!-- .site-navigation .main-navigation -->
 	</header>
 	<div id="main" class="site-main">
--- wp-content/themes/K.I.S.S.it/index.php
+++ wp-content/themes/K.I.S.S.it/index.php
@@ -9,13 +9,29 @@
  * Learn more: http://codex.wordpress.org/Template_Hierarchy
  *
  * @package K.I.S.S.it
+ * @since K.I.S.S.it 1.0
  */
 
 get_header(); ?>
 
 	<div id="primary" class="content-area">
 		<div id="content" class="site-content" role="main">
-		
+            <?php if(have_posts()): ?>
+                <?php k_i_s_s_it_content_nav( 'nav-above' ); ?>
+                <?php while(have_posts()) : the_post(); ?>
+                    <?php
+                        /**
+                         * include the Post-Format-specific template for the content
+                         * if you want to overlook this in a child theme then include a file
+                         * called content-__.php (where __ is the Post Format name) and that will be used instead
+                         */
+                         get_template_part('content', get_post_format());
+                    ?>
+                <?php endwhile; ?>
+                <?php k_i_s_s_it_content_nav( 'nav-below' ); ?>
+                <?php else : ?>
+                    <?php get_template_part( 'no-results', 'index' ); ?>
+            <?php endif; ?>
 		</div><!-- #content .site-content -->
 	</div><!-- #primary .content-area -->
 
